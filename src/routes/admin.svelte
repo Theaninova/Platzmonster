c69ff620c553997f789bfbb10ce1f0e8eb55e1d6
wassre <73964914+wassre@users.noreply.github.com>
2022-06-08 22:33:50 +0200

    AdminPage + Userlist (Frontend)
    Added Frontend für Admin Page, inklusive Component2 welche Userlist enthält.
    
    Co-Authored-By: simon-odp <57479054+simon-odp@users.noreply.github.com>

---
+++ src/routes/admin.svelte
@@ -0,0 +1,11 @@
+<script>
+
+    import Component2 from "../lib/components/Component2.svelte";
+</script>
+
+
+
+
+<Component2>
+
+</Component2>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,18 @@
 <script>
+  import Component2 from "../lib/components/Component2.svelte";
+
   export let users = [
     "user1",
     "user2",
     "user4",
   ]
 </script>
-
 <h1>Platzmonster</h1>
 
+
+
+<!--
 {#each users as user}
   <p>{user}</p>
 {/each}
+-->
