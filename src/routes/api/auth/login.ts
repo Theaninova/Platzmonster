ab71ae943701a072bb7730c125af956d75c80133
Thea Schöbl <dev@theaninova.de>
2022-06-12 22:13:19 +0200

    Reformat

--- src/routes/api/auth/login.ts
+++ src/routes/api/auth/login.ts
@@ -1,18 +1,13 @@
 import type {RequestHandler} from "@sveltejs/kit"
 import {logIn} from "../../../lib/session/sessionHandler"
 import {MOCK_ADMIN} from "../../../lib/models/mock/admin"
-import { User } from "$lib/models/user"
+import {User} from "$lib/models/user"
 
 export const post: RequestHandler = async ({request}) => {
-  
   const form = await request.formData()
-   
-  
-
 
   const cookies = await logIn(form.get("uname") as string, form.get("psw") as string)
-  
-  
+
   return cookies
     ? {
         status: 302,
--- src/routes/register.svelte
+++ src/routes/register.svelte
@@ -11,82 +11,81 @@
     <option value="admin">Admin</option>
 
     required />
-
   </select>
   <br /><br />
 
-    <label for="regusername">Username</label><br />
-    <input
-      id="regusername"
-      type="text"
-      name="name"
-      maxlength="30"
-      size="30"
-      placeholder="Wähle einen Usernamen aus"
-      required
-    /><br /><br />
+  <label for="regusername">Username</label><br />
+  <input
+    id="regusername"
+    type="text"
+    name="name"
+    maxlength="30"
+    size="30"
+    placeholder="Wähle einen Usernamen aus"
+    required
+  /><br /><br />
 
-    <label for="regvorname">Vorname</label><br />
-    <input
-      id="regvorname"
-      type="text"
-      name="vorname"
-      maxlength="30"
-      size="30"
-      placeholder="Gebe deinen Vornamen ein"
-      required
-    /><br /><br />
+  <label for="regvorname">Vorname</label><br />
+  <input
+    id="regvorname"
+    type="text"
+    name="vorname"
+    maxlength="30"
+    size="30"
+    placeholder="Gebe deinen Vornamen ein"
+    required
+  /><br /><br />
 
-    <label for="regnachname">Nachname</label><br />
-    <input
-      type="text"
-      id="regnachname"
-      name="nachname"
-      maxlength="30"
-      size="30"
-      placeholder="Gebe deinen Nachnamen ein"
-    /><br /><br />
+  <label for="regnachname">Nachname</label><br />
+  <input
+    type="text"
+    id="regnachname"
+    name="nachname"
+    maxlength="30"
+    size="30"
+    placeholder="Gebe deinen Nachnamen ein"
+  /><br /><br />
 
-    <label for="regemail">Email</label><br />
-    <input
-      id="regemail"
-      type="email"
-      name="email"
-      maxlength="30"
-      size="30"
-      placeholder="Gebe deine E-Mail-Adresse an"
-      required
-    /><br /><br />
+  <label for="regemail">Email</label><br />
+  <input
+    id="regemail"
+    type="email"
+    name="email"
+    maxlength="30"
+    size="30"
+    placeholder="Gebe deine E-Mail-Adresse an"
+    required
+  /><br /><br />
 
-    <label for="regpassword">Passwort</label><br />
-    <input
-      id="regpassword"
-      type="password"
-      name="password"
-      minlength="8"
-      maxlength="30"
-      size="30"
-      placeholder="Wähle ein Passwort aus"
-      required
-    /><br /><br />
+  <label for="regpassword">Passwort</label><br />
+  <input
+    id="regpassword"
+    type="password"
+    name="password"
+    minlength="8"
+    maxlength="30"
+    size="30"
+    placeholder="Wähle ein Passwort aus"
+    required
+  /><br /><br />
 
-    <label for="passwordConfirmation">Passwort wiederholen</label><br />
-    <input
-      id="passwordConfirmation"
-      type="password"
-      name="passwordConfirmation"
-      minlength="8"
-      maxlength="30"
-      size="30"
-      placeholder="Wiederhole das Passwort"
-      required
-    /><br />
+  <label for="passwordConfirmation">Passwort wiederholen</label><br />
+  <input
+    id="passwordConfirmation"
+    type="password"
+    name="passwordConfirmation"
+    minlength="8"
+    maxlength="30"
+    size="30"
+    placeholder="Wiederhole das Passwort"
+    required
+  /><br />
 
-    <label for="agb" /><br />
-    <input type="checkbox" id="agb" name="agb" required />Mit der Anmeldung bestätige ich, dass ich die
-    Allgemeinen Geschäftsbedingungen akzeptiere und die Datenschutzrichtlinien gelesen habe.<br /><br />
+  <label for="agb" /><br />
+  <input type="checkbox" id="agb" name="agb" required />Mit der Anmeldung bestätige ich, dass ich die
+  Allgemeinen Geschäftsbedingungen akzeptiere und die Datenschutzrichtlinien gelesen habe.<br /><br />
 
-    <button type="submit"> Account erstellen </button>
+  <button type="submit"> Account erstellen </button>
 </form>
 
 <style>
