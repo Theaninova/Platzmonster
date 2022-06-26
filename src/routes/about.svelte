25afb225a74b6ff200133ddda3e5f5abb792db5b
Melvin <=>
2022-06-26 22:00:49 +0200

    Initial commit about page

---
+++ src/routes/about.svelte
@@ -0,0 +1,91 @@
+<script></script>
+
+<h1>Über uns</h1>
+
+<div class="content"> 
+    <p class = "about-text">
+        Deine Pläne produktiv zu lernen werden davon gestört,
+         dass du in der Bibliothek erstmal 30 Minuten nach einem freien Platz suchen musst?
+         Wir das Platzmonster-Team haben uns es als Ziel gesetzt dem endgültig ein Ende zu setzen.
+    </p>
+
+    <p>
+        Wir sind ein junges Team von Studenten, welches Studenten den Stress der Arbeitsplatzsuche ersparen will mit Features wie:
+        <ul class="card">
+            <li>Registrierung</li>
+            <li>Login</li>
+            <li>Admin Page</li>
+            <li>uvm.</li>
+        </ul>
+
+        <p>weitere Features die geplant sind:
+            <ul class="card">
+                <li>Alles rund um die Arbeitsplatzreservierung</li>
+                <li>Kleinere Sachen für bessere Usability </li>
+            </ul>
+
+            
+</div>
+<br><br><br><br><br>
+
+<div class="team">
+    <h4>Mitwirkende</h4>
+<table class="table">
+    <tr>
+        <th>Name</th>
+        <th>Rolle</th>
+    </tr>
+
+    <tr>
+        <td>Thea Schöbl</td>
+        <td>Product-Owner</td>
+    </tr>
+    <tr>
+        <td>Alexander Tambunan</td>
+        <td>Scrum-Master</td>
+    </tr>
+    <tr>
+        <td>Simon Ostendorp</td>
+        <td>Entwickler</td>
+    </tr>
+    <tr>
+        <td>Hoang Duc Nguyen</td>
+        <td>Entwickler</td>
+    </tr>
+    <tr>
+        <td>Melvin Williams</td>
+        <td>Entwickler</td>
+    </tr>
+
+</table>
+
+</div>
+
+<style>
+    .content{
+        width: 100%;
+        text-align: center;
+    }
+
+    .card {
+    background: white;
+    color: black;
+    max-width: 400px;
+    margin-inline: auto;
+
+    padding: 16px;
+
+    display: grid;
+    gap: 4px;
+  }
+ 
+  th, td {
+    width: 80%;
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #DDD;
+}
+
+tr:hover {background-color: darkblue;}
+
+</style>
