e7fd4c197dfca53155129ccc75b4485e0bed0aa4
Melvin <=>
2022-07-04 16:21:22 +0200

    add temporary place search and corresponding place list item

---
+++ src/lib/components/types/list/PlaceListItem.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import type {IPlace} from "../../../models/place"
+    import {PlaceType} from "../../../models/place.js"
+  
+    export let item: IPlace
+  
+    const placeTypeTitles = {
+      [PlaceType.BUILDING]: "Gebäude",
+      [PlaceType.ROOM]: "Raum",
+      [PlaceType.WORK_PLACE]: "Arbeitsplatz",
+    }
+  </script>
+  
+  <div style="text-overflow: ellipsis">
+    <h3>
+      <abbr class="icon-24" title={placeTypeTitles[item.type]}
+        >{item.type === PlaceType.BUILDING ? "house" : item.type === PlaceType.ROOM ? "meeting_room" : "laptop_chromebook"}</abbr
+      >
+      <a href="/user/{item._id}"><abbr title={item._id}>{item.name}</abbr></a>
+    </h3>
+    <p class="subtitle">{item.name}</p>
+    <p>{item.description}</p>
+    <p>{item.parentId}</p> <!-- hier nochmal nachfragen was zu tun ist -->
+  </div>
+  
+  <style lang="scss">
+    h3 {
+      margin-block-end: 0;
+    }
+  
+    p {
+      margin-block: 4px;
+    }
+  
+    abbr {
+      text-decoration: none;
+    }
+  
+    .icon-24 {
+      cursor: help;
+    }
+  
+    h3 {
+      display: flex;
+      align-items: center;
+      gap: 4px;
+    }
+  
+    a {
+      text-decoration: none;
+      color: black;
+    }
+  
+    a:hover {
+      text-decoration: underline black;
+    }
+  </style>
---
+++ src/routes/api/place/search.ts
@@ -0,0 +1,37 @@
+import type {RequestHandler} from "@sveltejs/kit"
+import {Place, PlaceType} from "../../../lib/models/place"
+import type {IPlace} from "../../../lib/models/place"
+import {searchFormNames} from "../../../lib/models/form-names/search"
+
+export interface SearchResult<T> {
+  results: T[]
+  count: number
+  page: number
+  entriesPerPage: number
+}
+
+export const post: RequestHandler<Record<string, string>, SearchResult<IPlace>> = async ({
+  request,
+  locals,
+}) => {
+  const form = await request.formData()
+  const query = (form.get(searchFormNames.search) as string) || ""
+  const page = Number.parseInt(form.get(searchFormNames.page) as string) || 0
+  const entriesPerPage = Number.parseInt(form.get(searchFormNames.entriesPerPage) as string) || 20
+
+  const action = query === "*" ? {} : {$text: {$search: query}}
+
+  const count = await Place.find(action).countDocuments()
+  const result = await Place.find(action, locals.user?.userType === PlaceType.ADMIN ? {} : {password: 0})
+    .skip(page * entriesPerPage)
+    .limit(entriesPerPage)
+
+  return {
+    body: {
+      results: JSON.parse(JSON.stringify(result)),
+      count,
+      page,
+      entriesPerPage,
+    },
+  }
+}
