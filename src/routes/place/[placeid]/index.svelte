04ac2df1c61671321de43e73ff02f8579e2999fc
wassre <73964914+wassre@users.noreply.github.com>
2022-07-10 18:13:55 +0200

    Added Place Detail Page

---
+++ src/routes/place/[placeid]/index.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+  import type {IPlace} from "../../../lib/models/place"
+
+
+  export let item: IPlace
+</script>
+
+
+
+<h1>{item.shortName}</h1>
+
+<div class="card">
+  <h2>{item.name}</h2>
+
+  <table>
+    <tr>
+      <th>Art</th>
+      <td>{item.type}</td>
+    </tr>
+    <tr>
+      <th>Adresse</th>
+      <td>{item.address}</td>
+    </tr>
+    <tr>
+      <th> Beschreibung </th>
+      <td> {item.description}</td>
+    </tr>
+  </table>
+</div>
+
+<style>
+  .card {
+    background: white;
+    margin-inline: auto;
+    max-width: 16.5cm;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  table,
+  h2 {
+    margin-inline: auto;
+  }
+
+  table {
+    width: min-content;
+  }
+
+  th,
+  td {
+    text-align: left;
+  }
+</style>
---
+++ src/routes/place/[placeid]/index.ts
@@ -0,0 +1,24 @@
+import type {RequestHandler} from "./__types/index"
+import {Place, PlaceType} from "../../../lib/models/place"
+import {Rating} from "../../../lib/models/rating";
+
+
+export const get: RequestHandler = async ({params, locals}) => {
+  const place = await Place.findById(params.placeid)
+  const rating = await Rating.findById(params.placeid)
+
+  if (place) {
+    return {
+      body: {
+        item: JSON.parse(JSON.stringify(place)),
+        rating: JSON.parse(JSON.stringify(rating))
+
+      },
+    }
+  } else {
+    return {
+      status: 404,
+    }
+  }
+}
+
