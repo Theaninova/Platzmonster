a7bbb32ce9d66d1a3f7fba62672d7da09c843313
Thea Schöbl <dev@theaninova.de>
2022-07-10 18:03:43 +0200

    Admin page edit action

--- src/routes/admin.svelte
+++ src/routes/admin.svelte
@@ -5,12 +5,12 @@
 
   import SearchPage from "../lib/components/search/SearchPage.svelte"
   import UserListItem from "../lib/components/types/list/UserListItem.svelte"
-  import AddFriendAction from "../lib/components/types/actions/AddFriendAction.svelte"
+  import EditUserAction from "../lib/components/types/actions/EditUserAction.svelte"
 </script>
 
 <h1>Admin Panel</h1>
 
-<SearchPage action="/api/user/search" listItem={UserListItem} actionButtons={AddFriendAction} />
+<SearchPage action="/api/user/search" listItem={UserListItem} actionButtons={EditUserAction} />
 
 <!--
 <UserList {users} />
