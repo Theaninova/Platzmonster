8a6f6d77b0c3c40880df07c50ea9b7166b9c64b2
Thea Schöbl <dev@theaninova.de>
2022-07-15 22:18:33 +0200

    add cloudbuild config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,9 @@
 import {defineConfig} from "cypress"
-import cypressMongo from "cypress-mongodb"
 
 export default defineConfig({
   e2e: {
     specPattern: "cypress/**/*.spec.{ts,js}",
     baseUrl: "http://localhost:3000",
-    setupNodeEvents(on, config) {
-      void cypressMongo.configurePlugin(on)
-    },
     env: {
       mongodb: {
         uri: "mongodb://localhost:27017",
